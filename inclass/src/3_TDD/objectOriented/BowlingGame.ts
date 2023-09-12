670c995f4a75c4d33dbdf45b473dea14c46729eb
pegaso <jonathangogbz@gmail.com>
2023-09-11 20:50:30 -0600

    Se implementó la funcionalidad en programación orientada a objetos

--- inclass/src/3_TDD/objectOriented/BowlingGame.ts
+++ inclass/src/3_TDD/objectOriented/BowlingGame.ts
@@ -1,6 +1,50 @@
-
 export class BowlingGame {
+  private rolls: number[] = [];
 
-  constructor() { }
+  constructor() {}
 
+  private calculateStrikeScore(rollIndex: number): number {
+    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  }
+
+  private isStrike(rollIndex: number): boolean {
+    return this.rolls[rollIndex] === 10;
+  }
+
+  private calculateFrameScore(rollIndex: number): number {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  }
+
+  private calculateSpareScore(rollIndex: number): number {
+    return 10 + this.rolls[rollIndex + 2];
+  }
+
+  private isSpare(rollIndex: number): boolean {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
+  }
+
+  roll(pins: number): void {
+    this.rolls.push(pins);
+  }
+
+  score(): number {
+    const FRAMES = 10;
+    let totalScore = 0;
+    let rollIndex = 0;
+
+    for (let frame = 0; frame < FRAMES; frame++) {
+      if (this.isStrike(rollIndex)) {
+        totalScore += this.calculateStrikeScore(rollIndex);
+        rollIndex += 1;
+      } else if (this.isSpare(rollIndex)) {
+        totalScore += this.calculateSpareScore(rollIndex);
+        rollIndex += 2;
+      } else {
+        totalScore += this.calculateFrameScore(rollIndex);
+        rollIndex += 2;
+      }
+    }
+
+    return totalScore;
+  }
 }
